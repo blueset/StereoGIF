bfd175e7cd4f98aa7d72079facd5ae1c0d7aa020
Blueset <ilove@1a23.com>
2014-05-18 11:55:07 +0800

    Working System Control camera and picture save.

--- src/com/bluesetStudio/stereogif/MainActivity.java
+++ src/com/bluesetStudio/stereogif/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends Activity {
 
@@ -17,8 +17,8 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        
-        Button buttonShot = (Button) findViewById(R.id.button_shot);
+        //Button onclick
+        ImageButton buttonShot = (ImageButton) findViewById(R.id.button_shot);
         buttonShot.setOnClickListener(new OnClickListener() {
             
             @Override
